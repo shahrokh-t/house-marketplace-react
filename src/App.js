437e4272fdf467342c9deb9483f9367431b127d0
shahrokh-t <tahmoores54@gmail.com>
2023-03-20 12:09:51 +1100

    Add Contact Landlord page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp';
 import Navbar from './components/Navbar';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -22,13 +23,15 @@ function App() {
           <Route path='/offer' element={<Offers />} />
           <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
-            <Route path='/profile' element={<Profile />} /></Route>
-            {/* <Route path="/profile" element={<PrivateRoute> <Profile /> </PrivateRoute>} /> */}
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          {/* <Route path="/profile" element={<PrivateRoute> <Profile /> </PrivateRoute>} /> */}
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
-          <Route path="/category/:categoryName/:listingId" element={<Listing />}/>
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
